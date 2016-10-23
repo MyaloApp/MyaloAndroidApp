acedd71e12bbcd39a9622d75dc9caf355f5a7ebe
Kaylie Lu <k6lu@ucsd.edu>
2016-10-22 21:33:38 -0700

    initial

---
+++ app/src/main/java/com/example/kaylie/myaloandroidapp/TabsActivity.java
@@ -0,0 +1,13 @@
+package com.example.kaylie.myaloandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabs);
+    }
+}
